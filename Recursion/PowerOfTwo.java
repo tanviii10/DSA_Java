af64de9c334617d35c47667769ccb35cf2d9e044
Tanvi jagtap <tanvijagtap910@gmail.com>
2025-12-24 16:36:25 +0530

    Update PowerOfTwo.java

--- Recursion/PowerOfTwo.java
+++ Recursion/PowerOfTwo.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class PowerOfTwo {
+
+    public static int power(int n){
+
+        //base case
+        if(n==0){
+            return 1;
+        }
+
+        int smallpro=power(n-1);
+        int bigpro= 2 * smallpro;
+
+        return bigpro;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter thr power");
+        int n=sc.nextInt();
+
+        System.out.println("2 to the power "+ n+ " is "+ power(n));
+        sc.close();
+    }
     
 }
