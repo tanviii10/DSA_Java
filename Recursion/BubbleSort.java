ff59cb7d9ae765bd9134f136748b0294dbec2795
Tanvi jagtap <tanvijagtap910@gmail.com>
2025-12-25 18:58:15 +0530

    Create BubbleSort.java

---
+++ Recursion/BubbleSort.java
@@ -0,0 +1,33 @@
+public class BubbleSort {
+
+    public static void sort(int[] arr, int n) {
+
+        // Base case
+        if (n == 1) {
+            return;
+        }
+
+        // One pass of bubble sort
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+
+        // Recursive call with reduced size
+        sort(arr, n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 6, 7, 10, 3, 1};
+
+        sort(arr, arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
