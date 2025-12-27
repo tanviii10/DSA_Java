bcb36e92646cf6ebdf5a2b528ebf4e6b786f417e
Tanvi jagtap <tanvijagtap910@gmail.com>
2025-12-27 11:42:31 +0530

    Update Subsets.java

--- Recursion/Subsets.java
+++ Recursion/Subsets.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+
 public class Subsets {
 
-    
-    
+    static void solve(int[] arr, int index, ArrayList<Integer> output) {
+
+        // Base case
+        if (index == arr.length) {
+            System.out.println(output);
+            return;
+        }
+
+        // Exclude
+        solve(arr, index + 1, output);
+
+        // Include 
+        output.add(arr[index]);
+        solve(arr, index + 1, output);
+
+        // Backtrack
+        output.remove(output.size() - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3};
+        ArrayList<Integer> output = new ArrayList<>();
+
+        solve(arr, 0, output);
+    }
 }
