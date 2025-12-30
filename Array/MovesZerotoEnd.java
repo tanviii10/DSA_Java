3683203be9c8dfd8c2e926e47b5acb98679febb2
Tanvi jagtap <tanvijagtap910@gmail.com>
2025-12-30 18:33:47 +0530

    Update MovesZerotoEnd.java

--- Array/MovesZerotoEnd.java
+++ Array/MovesZerotoEnd.java
@@ -1,19 +1,21 @@
 public class MovesZerotoEnd {
 
     static void move(int arr[]){
+        
         int j=-1;
-        for(int i=0 ; i< arr.length ; i++){
-            if( arr[i] == 0){
-                j = i;
+        
+        for( int i=0; i< arr.length ; i++){
+            if(arr[i] == 0){
+                j=i;
                 break;
             }
         }
 
-        if(j== -1) return;
+        if( j == -1) return;
 
-        for(int i =j+1 ; i< arr.length ;i++){
-            if(arr[i] !=0){
-                int temp=arr[i];
+        for( int i=j+1; i< arr.length ; i++){
+            if( arr[i] != 0){
+                int temp= arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
                 j++;
