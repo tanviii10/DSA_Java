2b8f0dc8e9216e867c897b6284fa6338a5d96184
Tanvi jagtap <tanvijagtap910@gmail.com>
2025-12-30 10:51:53 +0530

    update

--- Basics/Loops.java
+++ Basics/Loops.java
@@ -1,4 +1,4 @@
-public class dfor {
+public class Loops {
     public static void main(String[] args) {
         
         // for(int i=1 ;i<=5; i++){
--- Basics/Switch.java
+++ Basics/Switch.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Dswitch {
+public class Switch {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
          
