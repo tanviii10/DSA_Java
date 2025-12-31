716c78e78393ef734f5e8fdb989910875eec17a6
Tanvi jagtap <tanvijagtap910@gmail.com>
2025-12-31 19:30:01 +0530

    Update ReverseWordInCharArray.java

--- Strings/ReverseWordInCharArray.java
+++ Strings/ReverseWordInCharArray.java
@@ -16,17 +16,14 @@ public class ReverseWordInCharArray {
     }
     public static void reverseWords( char s[]){
 
-        //1. Reverse entire array
         reverse( s,0, s.length-1);
 
-        // 2. Reverse each word inside array
         int start=0;
-        for(int end=0; end <= s.length ; end++){
+        for(int i=0; i<= s.length ; i++){
 
-            // When space or end of array → reverse previous word
-            if( end== s.length || s[end] == ' '){
-                reverse(s, start, end-1);
-                start= end +1; // Move to next word
+                if( i == s.length || s[i] == ' '){
+                reverse(s, start, i-1);
+                start= i + 1;
             }
         }
     }
