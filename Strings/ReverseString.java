a56ffdbddae30b88f2d346a19ddcb1d7261ffa92
Tanvi jagtap <tanvijagtap910@gmail.com>
2025-12-31 19:14:15 +0530

    Update ReverseString.java

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -1,23 +1,26 @@
-import java.util.Scanner;
-
 public class ReverseString {
 
-    public static String Reverse(String s){
+    public static String Reverse(char arr[]){
 
-        String rev="";
-        for( int i=s.length()-1 ; i>= 0; i--){
-            rev= rev + s.charAt(i);
+        int left=0;
+        int right=arr.length-1;
+
+        while( left < right){
+            char temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
         }
-        return rev;
+        return new String(arr);
     }
 
     public static void main(String[] args) {
         
-        Scanner sc=new Scanner(System.in);
-        String s= sc.nextLine();
+        String str="tanvi";
+        char[] arr = str.toCharArray();
 
-        System.out.println(Reverse(s));
-        sc.close();
+        System.out.println(Reverse(arr));
     }
     
 }
