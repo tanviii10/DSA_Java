ec7ad4e0164611b19afe9797d4c83bd56427007d
Tanvi jagtap <tanvijagtap910@gmail.com>
2026-01-03 16:18:39 +0530

    Create ReverseLLitrative.java

---
+++ LinkedList/ReverseLLitrative.java
@@ -0,0 +1,49 @@
+public class ReverseLLitrative {
+
+    public static class Node{
+
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    
+    public static Node head=null;
+
+    public static void reverse(){
+        Node prev=null;
+        Node curr=head;
+        Node forward=null;
+
+        while( curr != null ){
+            forward = curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        head = prev;
+    }
+
+    static void print(){
+        Node temp=head;
+        while( temp != null){
+            System.out.print(temp.data+ " -> ");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        
+        head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        
+        print();
+        reverse();
+        print();
+    }
+}
