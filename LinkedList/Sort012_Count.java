e6d418e5477b5f0a22e30e88fd3861f12d2a39ec
Tanvi jagtap <tanvijagtap910@gmail.com>
2026-01-04 21:45:36 +0530

    Create Sort012_Count.java

---
+++ LinkedList/Sort012_Count.java
@@ -0,0 +1,73 @@
+public class Sort012_Count {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static Node head = null;
+
+    static void sort012(){
+
+        int zeroCount=0, oneCount=0, twoCount=0;
+        Node temp=head;
+
+        while( temp != null){
+
+            if( temp.data == 0) 
+                zeroCount++;
+            else if( temp.data == 1) 
+                oneCount++;
+            else 
+                twoCount++;
+
+            temp=temp.next;
+        }
+
+        temp=head;
+        while( temp != null){
+
+            if( zeroCount != 0){
+                temp.data=0;
+                zeroCount--;
+            }
+            else if( oneCount != 0){
+                temp.data=1;
+                oneCount--;
+            }
+            else{
+                temp.data=2;
+                twoCount--;
+            }
+            temp=temp.next;
+        }
+    }
+
+    static void print() {
+        Node t = head;
+        while (t != null) {
+            System.out.print(t.data + " -> ");
+            t = t.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        head = new Node(1);
+        head.next = new Node(0);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(1);
+        head.next.next.next.next = new Node(0);
+
+        print();
+        sort012();
+        print();
+    }
+    
+}
