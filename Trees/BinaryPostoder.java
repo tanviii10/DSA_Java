25763fec2e5d7102849628dd54c8ac662a1d45b3
Tanvi jagtap <tanvijagtap910@gmail.com>
2026-03-15 09:21:38 +0530

    Update BinaryPostoder.java

--- Trees/BinaryPostoder.java
+++ Trees/BinaryPostoder.java
@@ -2,58 +2,51 @@ import java.util.Scanner;
 
 public class BinaryPostoder {
 
-    static class Node {
+    static class Node{
         int data;
         Node left;
         Node right;
 
-        Node(int data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
         }
     }
 
-    static Scanner sc = new Scanner(System.in);
+    static Scanner sc=new Scanner(System.in);
+    static Node buildTree(){
 
-    static Node buildTree() {
-        int data = sc.nextInt();
-
-        if (data == -1) {
+        int data=sc.nextInt();
+        if(data == -1){
             return null;
         }
 
-        Node root = new Node(data);
+        Node root=new Node(data);
 
-        System.out.println("Enter left of " + data);
-        root.left = buildTree();
+        System.out.println("enter left of : "+data);
+        root.left=buildTree();
 
-        System.out.println("Enter right of " + data);
-        root.right = buildTree();
+        System.out.println("enter right of " +data);
+        root.right=buildTree();
 
         return root;
     }
 
-    static void postorder(Node root) {
+    static void postorder(Node root){
 
-    // base case
-    if (root == null) {
-        return;
+        if(root == null){
+            return;
+        }
+
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data+" ");
     }
 
-    // Left
-    postorder(root.left);
-
-    // Right
-    postorder(root.right);
-
-    // Root
-    System.out.print(root.data + " ");
-
-    }
     public static void main(String[] args) {
 
-        System.out.println("Enter data (-1 for NULL):");
+        System.out.println("Enter data :");
 
         Node root = buildTree();
 
