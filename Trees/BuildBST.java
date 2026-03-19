2bcaa71f30a8f3c22436724835f231e44356e61b
Tanvi jagtap <tanvijagtap910@gmail.com>
2026-03-19 18:28:03 +0530

    Create BuildBST.java

---
+++ Trees/BuildBST.java
@@ -0,0 +1,93 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BuildBST {
+
+    static class  Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static void levelOrder(Node root){
+
+        if(root == null){
+            return;
+        }
+
+        Queue<Node> q=new LinkedList<>();
+
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Node temp=q.poll();
+
+            if(temp == null){
+                System.out.println();
+
+                if(!q.isEmpty()){
+                    q.add(null);
+                }
+            }
+            else{
+                System.out.print(temp.data+" ");
+
+                if(temp.left != null){
+                    q.add(temp.left);
+                }
+                if(temp.right != null){
+                    q.add(temp.right);
+                }
+            }
+        }
+
+    }
+    static Node insertIntoBST(Node root, int data) {
+
+        if (root == null) {
+            return new Node(data);
+        }
+
+        if (data < root.data) {
+            root.left = insertIntoBST(root.left, data);
+        } else {
+            root.right = insertIntoBST(root.right, data);
+        }
+
+        return root;
+    }
+
+    static Node insertIntoBST(Node root) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter data To build BST:");
+
+        int data = sc.nextInt();
+
+        while (data != -1) {
+            root = insertIntoBST(root, data);
+            data = sc.nextInt();
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+
+        Node root = null;
+
+        root = insertIntoBST(root);
+        
+        System.out.println("printing BST : ");
+        levelOrder(root);
+
+    }
+}
