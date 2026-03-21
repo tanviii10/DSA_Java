f5957b9b83709da5871e85384c981889ba041eea
Tanvi jagtap <tanvijagtap910@gmail.com>
2026-03-21 10:40:22 +0530

    Create Graph.java

---
+++ Graphs/Graph.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Graph{
+
+    // Function to add edge
+    public static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u); // undirected
+    }
+
+    // Function to print graph
+    public static void printGraph(ArrayList<ArrayList<Integer>> adj, int V) {
+        for (int i = 0; i < V; i++) {
+            System.out.print(i + " -> ");
+            
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.print(adj.get(i).get(j) + " ");
+            }
+            
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of vertices: ");
+        int V = sc.nextInt();
+
+        System.out.print("Enter number of edges: ");
+        int E = sc.nextInt();
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        // Initialize adjacency list
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        System.out.println("Enter edges (u v): ");
+
+        // Input edges
+        for (int i = 0; i < E; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            addEdge(adj, u, v);
+        }
+
+        // Print graph
+        printGraph(adj, V);
+    }
+}
