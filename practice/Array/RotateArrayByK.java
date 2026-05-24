793e5eab39615279b32e78b1332ea6b5c57d8945
Tanvi jagtap <tanvijagtap910@gmail.com>
2026-05-24 17:33:48 +0530

    new

--- practice/Array/RotateArrayByK.java
+++ practice/Array/RotateArrayByK.java
@@ -1,5 +1,31 @@
 public class RotateArrayByK {
     public static void reverse(int arr[], int start, int end){
-        
+        while( start < end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+
+            start++;
+            end--;
+        }
+    }
+    public static void rotate(int arr[], int k){
+
+        int n=arr.length;
+        k=k % n;
+
+        reverse(arr, 0, n-1);
+        reverse(arr, 0, k-1);
+        reverse(arr, k , n-1);
+    }
+
+    public static void main(String args[]){
+        int arr[]={ 1,2,3,4,5};
+        int k=2;
+        rotate( arr , k);
+
+        for(int i=0; i< arr.length ; i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
